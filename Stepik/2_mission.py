0e15d364b9017520f83ac801b68f108c5b7d5c60
Vlad Filipovich <fvfordota2@gmail.com>
2022-08-13 16:02:53 +0300

    PEP8 (2_mission)

--- Stepik/2_mission.py
+++ Stepik/2_mission.py
@@ -2,15 +2,16 @@
 from random import *
 
 answers = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.",
-           "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.",
-           "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.",
-           "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.",
-           "Yes – definitely.", "You may rely on it."]
+           "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.",
+           "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
+           "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.",
+           "You may rely on it."]
 print("Hello world, I'm Magic Ball 8 and I know all the answers", '\n')
 print("What's your name?")
 name = input()
 print("Nice to meet you,", name)
 print("Now you can ask me anything!!!")
+
 
 def magic_ball_8():
     while True:
@@ -24,4 +25,6 @@ def magic_ball_8():
         if Y_N.lower() == 'no':
             print("Come back if you have any questions!")
             break
+
+
 magic_ball_8()
