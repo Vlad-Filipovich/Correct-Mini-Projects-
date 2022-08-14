e737d2361a05079f7d7bc8868ee64b00f6c93fe9
Vlad Filipovich <fvfordota2@gmail.com>
2022-08-14 12:33:38 +0300

    Completed 4_mission

--- Stepik/4_mission.py
+++ Stepik/4_mission.py
@@ -3,20 +3,49 @@ eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
 eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-symbol = [" ", ",", ".", "!", "?"]
-keys = int(input())
-orig_str = [i for i in input()]
+print('Enter Process Variant (encrypt = "e" or decipher = "d":')
+var = input()
+print('Enter language ("rus" or "eng"):')
+lang = input()
+print('Enter shift step:')
+step = int(input())
+print('Enter start string:')
+start_str = [i for i in input()]
 
-for i in range(len(orig_str)):
-    if orig_str[i] in rus_upper_alphabet:
-        orig_str[i] = rus_upper_alphabet[(rus_upper_alphabet.index(orig_str[i]) + keys) % 32]
-    if orig_str[i] in rus_lower_alphabet:
-        orig_str[i] = rus_lower_alphabet[(rus_lower_alphabet.index(orig_str[i]) + keys) % 32]
-print(''.join(orig_str))
 
-for i in range(len(orig_str)):
-    if orig_str[i] in eng_upper_alphabet:
-        orig_str[i] = eng_upper_alphabet[(eng_upper_alphabet.index(orig_str[i]) + keys) % 26]
-    if orig_str[i] in eng_lower_alphabet:
-        orig_str[i] = eng_lower_alphabet[(eng_lower_alphabet.index(orig_str[i]) + keys) % 26]
-print(''.join(orig_str))
+def encrypt():  # зашифровать
+    if lang == 'rus':
+        for i in range(len(start_str)):
+            if start_str[i] in rus_upper_alphabet:
+                start_str[i] = rus_upper_alphabet[(rus_upper_alphabet.index(start_str[i]) + step) % 32]
+            if start_str[i] in rus_lower_alphabet:
+                start_str[i] = rus_lower_alphabet[(rus_lower_alphabet.index(start_str[i]) + step) % 32]
+        print(''.join(start_str))
+    if lang == 'eng':
+        for i in range(len(start_str)):
+            if start_str[i] in eng_upper_alphabet:
+                start_str[i] = eng_upper_alphabet[(eng_upper_alphabet.index(start_str[i]) + step) % 26]
+            if start_str[i] in eng_lower_alphabet:
+                start_str[i] = eng_lower_alphabet[(eng_lower_alphabet.index(start_str[i]) + step) % 26]
+        print(''.join(start_str))
+
+
+def decipher():  # расшифровать
+    if lang == 'rus':
+        for i in range(len(start_str)):
+            if start_str[i] in rus_upper_alphabet:
+                start_str[i] = rus_upper_alphabet[(rus_upper_alphabet.index(start_str[i]) - step) % 32]
+            if start_str[i] in rus_lower_alphabet:
+                start_str[i] = rus_lower_alphabet[(rus_lower_alphabet.index(start_str[i]) - step) % 32]
+        print(''.join(start_str))
+    if lang == 'eng':
+        for i in range(len(start_str)):
+            if start_str[i] in eng_upper_alphabet:
+                start_str[i] = eng_upper_alphabet[(eng_upper_alphabet.index(start_str[i]) - step) % 26]
+            if start_str[i] in eng_lower_alphabet:
+                start_str[i] = eng_lower_alphabet[(eng_lower_alphabet.index(start_str[i]) - step) % 26]
+        print(''.join(start_str))
+if var == 'e':
+    encrypt()
+if var == 'd':
+    decipher()
