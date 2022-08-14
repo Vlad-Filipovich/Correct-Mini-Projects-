47e24117ac307ba7e61d7f0ce6d4a49a100b14a7
Vlad Filipovich <fvfordota2@gmail.com>
2022-08-14 16:48:51 +0300

    Changes in 5_mission - 5_1_mission

--- Stepik/5_mission.py
+++ Stepik/5_mission.py
@@ -1,15 +1,18 @@
-start_list = ['88', '32', '22', '16', '17']
-for power in range(1, 11):
-    answer_list = []
-    for i in range(5):
-        n = start_list[i]
-        a = len(n) - 1
-        total = 0
-        for i in range(len(n)):
-            total += int(n[i]) * (power ** a)
-            a -= 1
-        answer_list.append(total)
-    if answer_list[0] == sum(answer_list[1:]):
-        break
-print(power)
-print(answer_list)
+# Перевод из десятичной системы счисления в заданную
+
+eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+answer = []
+print('Enter the number:')
+num = int(input())
+print('Enter the number system you need:')
+system = int(input())
+while num >= system:
+    ostatok = num % system
+    if ostatok > 9:
+        answer.append(eng_upper_alphabet[ostatok - 10])
+        num //= system
+    else:
+        answer.append(str(ostatok))
+        num //= system
+answer.append(str(num))
+print(''.join(answer[::-1]))
